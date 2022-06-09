8463bfb326f29d0ee73c9bed542e0cb3e242429c
jacc89 <laflacalex9@gmail.com>
2022-06-08 20:32:28 -0500

    inicio de proyecto semana4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompartidosModule } from './compartidos/compartidos.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    CompartidosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
